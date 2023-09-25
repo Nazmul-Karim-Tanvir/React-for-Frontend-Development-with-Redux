489dc0b6c56b726fb7021d2d55572a5123de9593
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2023-09-25 23:02:36 +0600

    impoer and export es6 feature practice

---
+++ component.js
@@ -0,0 +1,12 @@
+let a = 45;
+let str = "Tanvir";
+let obj = {
+    name: "Rahat", age:27
+}
+let arr = [1,2,5,4,"nk"];
+
+let hello = () =>{
+    console.log("hello world");
+}
+
+console.log(hello());
