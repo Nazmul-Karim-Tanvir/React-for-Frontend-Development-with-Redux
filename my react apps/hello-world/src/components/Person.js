8122c0eea308c9847d735cef135226f8be75c48c
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2023-09-29 00:52:49 +0600

    dinamic function practice done

--- my react apps/hello-world/src/components/Person.js
+++ my react apps/hello-world/src/components/Person.js
@@ -1,11 +1,13 @@
 import React from "react";
-function Person(){
+
+
+function Person(props) {
     return (
-      <div>
-        <h1>Hello World 2 From Person</h1>
-      </div>
+        <div>
+            <h1>Name:{props.name} Age: {props.age}</h1>
+            <h4>Contect: {props.children} </h4>
+        </div>
     );
-  
-  }
-  
-  export default Person;
+}
+
+export default Person;
