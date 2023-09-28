9ea0210cecb266385e02f3abfa59a2ace3beaec5
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2023-09-29 00:22:27 +0600

    function component vs class component practice

--- my react apps/hello-world/src/App.js
+++ my react apps/hello-world/src/App.js
@@ -1,12 +1,36 @@
-import React from "react";
+import React,{Component} from "react";
 import './App.css';
+import Person from "./components/Person";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+
+//function component 
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hello World</h1>
+//       <Person/>
+//     </div>
+//   );
+// }
+
+// class component 
+class App extends Component{
+  render(){
+    return (
+          <div className="App">
+            <h1>Hello World</h1>
+            <Person/>
+          </div>
+        );
+  }
 }
+
+// function Person(){
+//   return (
+//     <div>
+//       <h1>Hello World 2 From Person</h1>
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ my react apps/hello-world/src/components/Person.js
@@ -0,0 +1,11 @@
+import React from "react";
+function Person(){
+    return (
+      <div>
+        <h1>Hello World 2 From Person</h1>
+      </div>
+    );
+  
+  }
+  
+  export default Person;
