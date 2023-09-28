413904cb23ff6b0372c2b54d27f425a94b87e38a
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2023-09-29 01:12:40 +0600

    dynamic class component practice

--- my react apps/hello-world/src/App.js
+++ my react apps/hello-world/src/App.js
@@ -1,32 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 import './App.css';
 import Person from "./components/Person";
 
 
 //function component 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-      <Person name="Nazmul" age="25">01867064697</Person>
-      <Person name="Karim" age="27">01778814577</Person>
-      <Person name="Tanvir" age="25">0157546555</Person>
-      <Person name="Tani" age="27">1015451515</Person>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hello World</h1>
+//       <Person name="Nazmul" age="25">01867064697</Person>
+//       <Person name="Karim" age="27">01778814577</Person>
+//       <Person name="Tanvir" age="25">0157546555</Person>
+//       <Person name="Tani" age="27">1015451515</Person>
+//     </div>
+//   );
+// }
 
 // class component 
-// class App extends Component{
-//   render(){
-//     return (
-//           <div className="App">
-//             <h1>Hello World</h1>
-//             <Person/>
-//           </div>
-//         );
-//   }
-// }
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello World</h1>
+        <Person name="Nazmul" age="25">01867064697</Person>
+        <Person name="Karim" age="27">01778814577</Person>
+        <Person name="Tanvir" age="25">0157546555</Person>
+        <Person name="Tani" age="27">1015451515</Person>
+      </div>
+    );
+  }
+}
 
 // function Person(){
 //   return (
--- my react apps/hello-world/src/components/Person.js
+++ my react apps/hello-world/src/components/Person.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 
+// function component
+// function Person(props) {
+//     return (
+//         <div>
+//             <h1>Name:{props.name} Age: {props.age}</h1>
+//             <h4>Contect: {props.children} </h4>
+//         </div>
+//     );
+// }
 
-function Person(props) {
-    return (
-        <div>
-            <h1>Name:{props.name} Age: {props.age}</h1>
-            <h4>Contect: {props.children} </h4>
-        </div>
-    );
+//class component
+
+class Person extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                <h1>Name:{this.props.name} Age: {this.props.age}</h1>
+                <h4>Contect: {this.props.children} </h4>
+            </div>
+        );
+    }
 }
+
 
 export default Person;
