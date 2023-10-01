e628040c15ffdae47756f24f26c953500f99dbc0
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2023-10-01 17:00:38 +0600

    commit

--- my react apps/hello-world/src/App.js
+++ my react apps/hello-world/src/App.js
@@ -7,9 +7,9 @@ class App extends Component {
   //State
   state = {
     books: [
-      { bookName: "1984", writer: "George Orwell" },
-      { bookName: "The Da Vinci Code", writer: "Dan Brown" },
-      { bookName: "The Alchemist", writer: "Paulo Coelho" }
+      { id: 1, bookName: "1984", writer: "George Orwell" },
+      { id: 2, bookName: "The Da Vinci Code", writer: "Dan Brown" },
+      { id: 3, bookName: "The Alchemist", writer: "Paulo Coelho" }
     ]
   }
 
@@ -61,6 +61,7 @@ class App extends Component {
           bookName={book.bookName}
           writer={book.writer}
           delete={() => this.deleteBookState(index)}
+          key={book.id}
         />
       );
     });
