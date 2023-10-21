cb2ade34289291d3cf1bab6f51d3a1d342c7cba5
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2023-10-21 23:27:00 +0600

    2. Designing Navigation Bar

--- project-1-resturent-app/resturent-app/src/componenets/header/NavigationComponent.js
+++ project-1-resturent-app/resturent-app/src/componenets/header/NavigationComponent.js
@@ -1,18 +1,35 @@
-import React from 'react'
-import {Navbar,NavbarBrand} from "reactstrap";
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
+import { Collapse, Nav, NavItem, Navbar, NavbarBrand, NavbarToggler } from "reactstrap";
 const NavigationComponent = () => {
+    const [isNavOpen, setIsNavOpen] = useState(false);
+    const navToggle = () => {
+        setIsNavOpen(!isNavOpen);
+    };
     return (
         <div>
-            <Navbar
-                color="dark"
-                dark
-            >
-                <NavbarBrand href="/">
-                    Resturent
-                </NavbarBrand>
+            <Navbar color="dark" dark expand="sm">
+                <NavbarBrand href="/"> Resturent</NavbarBrand>
+                <NavbarToggler onClick={navToggle} />
+                <Collapse isOpen={isNavOpen} navbar>
+                    <Nav className='me-auto' navbar>
+                        <NavItem>
+                            <Link to="/" className="nav-link">Home</Link>
+                        </NavItem>
+                        <NavItem>
+                            <Link to="/menu" className="nav-link">Menu</Link>
+                        </NavItem>
+                        <NavItem>
+                            <Link to="/about" className="nav-link">About</Link>
+                        </NavItem>
+                        <NavItem>
+                            <Link to="/contact" className="nav-link">Contact</Link>
+                        </NavItem>
+                    </Nav>
+                </Collapse>
             </Navbar>
         </div>
     )
 }
 
-export default NavigationComponent
+export default NavigationComponent;
