c99c18b81cf812b3f3fb4438832695c8f5a42f3e
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2023-10-21 14:55:18 +0600

    1. Preparing project for Router

--- project-1-resturent-app/resturent-app/src/componenets/body/About.jsx
+++ project-1-resturent-app/resturent-app/src/componenets/body/About.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const BodyComponent = () => {
+const About = () => {
   return (
     <div>
-
+      
     </div>
   )
 }
 
-export default BodyComponent
+export default About
