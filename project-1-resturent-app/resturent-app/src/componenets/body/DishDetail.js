90f8b563e392456c44a1bf08a12d3a353d50d7a3
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2023-10-22 16:32:34 +0600

    Load Comments from comments.js

--- project-1-resturent-app/resturent-app/src/componenets/body/DishDetail.js
+++ project-1-resturent-app/resturent-app/src/componenets/body/DishDetail.js
@@ -1,37 +1,26 @@
 import React from 'react';
-import { Card, CardBody, CardImg, CardText, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardBody, CardTitle, CardText } from 'reactstrap';
 import LoadComments from './LoadComments';
 
-const DishDetail = ({ dish }) => {
-  return (
-    <div>
-      <Card className='my-2'>
-        <CardImg
-          alt={dish.name}
-          src={dish.image}
-          style={{
-            height: '100%'
-          }}
-          top
-          width='100%'
-        />
-        <CardBody style={{ textAlign: 'left' }}>
-
-          <CardTitle tag="h5">{dish.name}</CardTitle>
-          <CardText>
-            {dish.description}
-          </CardText>
-          <CardText>
-            {dish.price}
-          </CardText>
-          <hr />
-          <LoadComments comments={dish.comments}></LoadComments>
-
-        </CardBody>
-
-      </Card>
-    </div>
-  );
-};
+const DishDetail = props => {
+    return (
+        <div>
+            <Card style={{ marginTop: "10px" }}>
+                <CardImg top src={props.dish.image} alt={props.dish.name} />
+                <CardBody style={{ textAlign: "left" }}>
+                    <CardTitle>{props.dish.name}</CardTitle>
+                    <CardText>
+                        {props.dish.description}
+                    </CardText>
+                    <CardText>
+                        Price: {props.dish.price}/-
+                    </CardText>
+                    <hr />
+                    <LoadComments comments={props.comments}></LoadComments>
+                </CardBody>
+            </Card>
+        </div>
+    );
+}
 
 export default DishDetail;
