d9098da759352b8bf268046804d6ac32f4fdd685
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2023-10-29 23:56:45 +0600

    4. Dispatching Actions

---
+++ REDUX-BASIC/index.js
@@ -0,0 +1,39 @@
+const redux = require("redux");
+// console.log(redux);
+
+//State
+const initState = {
+    num: 0
+}
+
+//Reducer
+const rootReducer = (state = initState, action) => {
+    if(action.type === 'INC_NUM'){
+        return{
+            ...state,
+            num: state.num+1
+        }
+    }
+    if(action.type === 'ADD_NUM'){
+        return{
+            ...state,
+            num: state.num+action.value
+        }
+    }
+    return state;
+}
+
+const store = redux.createStore(rootReducer);
+console.log(store.getState());
+
+// Dispatching Action
+store.dispatch({
+    type: "INC_NUM"
+});
+
+store.dispatch({
+    type: "ADD_NUM",
+    value:34
+});
+
+console.log(store.getState());
