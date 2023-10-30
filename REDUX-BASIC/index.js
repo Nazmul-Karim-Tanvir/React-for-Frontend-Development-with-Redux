9e7d37b3334f6661c03879764ea250cde960a656
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2023-10-30 18:02:23 +0600

    5. Store Subscription

--- REDUX-BASIC/index.js
+++ REDUX-BASIC/index.js
@@ -8,32 +8,48 @@ const initState = {
 
 //Reducer
 const rootReducer = (state = initState, action) => {
-    if(action.type === 'INC_NUM'){
-        return{
+    if (action.type === 'INC_NUM') {
+        return {
             ...state,
-            num: state.num+1
+            num: state.num + 1
         }
     }
-    if(action.type === 'ADD_NUM'){
-        return{
+    if (action.type === 'ADD_NUM') {
+        return {
             ...state,
-            num: state.num+action.value
+            num: state.num + action.value
+        }
+    }
+    if (action.type === 'DEC_NUM') {
+        return {
+            ...state,
+            num: state.num - 1
         }
     }
     return state;
 }
 
+//Store
 const store = redux.createStore(rootReducer);
 console.log(store.getState());
 
+//Subscribe
+store.subscribe(() => {
+    console.log("[Subscribe]",store.getState());
+});
+
 // Dispatching Action
+store.dispatch({
+    type: "ADD_NUM",
+    value: 34
+});
+
 store.dispatch({
     type: "INC_NUM"
 });
 
 store.dispatch({
-    type: "ADD_NUM",
-    value:34
+    type: 'DEC_NUM'
 });
 
-console.log(store.getState());
+
