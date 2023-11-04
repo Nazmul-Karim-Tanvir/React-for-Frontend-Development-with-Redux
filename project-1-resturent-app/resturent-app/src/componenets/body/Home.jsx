72fb3933ddc3e55ee4628834047dc7037317c4ea
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2023-11-04 19:59:39 +0600

    4. Dispatching Action from Component

--- project-1-resturent-app/resturent-app/src/componenets/body/Home.jsx
+++ project-1-resturent-app/resturent-app/src/componenets/body/Home.jsx
@@ -2,26 +2,37 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux';
 
 const mapStateToProps = state => {
-  console.log("mapStateToProps: ", state);
+ // console.log("mapStateToProps: ", state);
   return {
-      dishes: state.dishes,
-      sample: state.sample
+    dishes: state.dishes,
+    sample: state.sample
   }
 }
 
 export class Home extends Component {
   componentDidMount() {
-    console.log("Home State: ", this.state);
     console.log("Home Props: ", this.props);
-}
+
+    this.props.dispatch({
+      type: 'TEST',
+      str: "Bohubrihi"
+    })
+
+  }
+
+  componentDidUpdate(){
+    console.log("Home Props Updated: ",this.props)
+  }
+
   render() {
-    document.title= "Home";
+    document.title = "Home";
     return (
       <div>
-        
+
       </div>
     )
   }
+
 }
 
 export default connect(mapStateToProps)(Home);
