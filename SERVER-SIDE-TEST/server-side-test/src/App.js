9d5f2177059fdb6ae88002ca51e36bf28016b8c3
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2023-11-15 23:35:00 +0600

    7. Handling Responses using State

--- SERVER-SIDE-TEST/server-side-test/src/App.js
+++ SERVER-SIDE-TEST/server-side-test/src/App.js
@@ -1,32 +1,48 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
-//import fetch from 'cross-fetch';
 import axios from 'axios';
 
-class App extends Component{
-  componentDidMount(){
-  //  fetch('https://jsonplaceholder.typicode.com/posts')
-  //  .then(response => response.json())
-  //  .then(data => console.log(data));
-  // axios.get('http://localhost:3001dishes')
-  // .then(response => response.data)
-  // .then(data => console.log(data))
-  // .catch(error => console.log(error.message));
+class App extends Component {
+  state = {
+    dishes: null,
+    errMess: null
+  }
+  componentDidMount() {
+    console.log("ComponentDidMount: ", this.state);
+    // axios.get('http://localhost:3001/dishes')
+    //   .then(response => response.data)
+    //   .then(data => {
+    //     this.setState({
+    //       dishes: data
+    //     })
+    //   })
+    //   .catch(error => {
+    //     this.setState({
+    //       errMess: error.message
+    //     })
+    //   });
 
-  // axios.post('http://localhost:3001/dishes', {name:"pizza",price:'799'})
-  // .then(response => console.log(response));
+    // axios.post('http://localhost:3001/dishes', { name: "Pizza", price: "799" })
+    //   .then(response => console.log(response))
 
-  // axios.put('http://localhost:3001/dishes/1', {name:"Cheese Cake",price:'250'})
-  // .then(response => console.log(response));
+    // axios.put('http://localhost:3001/dishes/1', { name: "Cheese Cake", price: "250" })
+    //   .then(response => console.log(response))
 
-  axios.delete('http://localhost:3001/dishes/5')
-  .then(response => console.log(response));
+    axios.delete('http://localhost:3001/dishes/0')
+      .then(response => console.log(response))
+      .catch(error => {
+        this.setState({
+          errMess: error.message
+        })
+      })
   }
 
-  render(){
-    return(
+  componentDidUpdate() {
+    console.log("Update: ", this.state);
+  }
+  render() {
+    return (
       <div>
-
 
       </div>
     );
