b6efcca1e2072bbf61244f221383089a9ad8c508
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2023-11-15 23:20:20 +0600

    6. Error Handling, POST, PUT, DELETE

--- SERVER-SIDE-TEST/server-side-test/src/App.js
+++ SERVER-SIDE-TEST/server-side-test/src/App.js
@@ -8,9 +8,19 @@ class App extends Component{
   //  fetch('https://jsonplaceholder.typicode.com/posts')
   //  .then(response => response.json())
   //  .then(data => console.log(data));
-  axios.get('https://jsonplaceholder.typicode.com/posts')
-  .then(response => response.data)
-  .then(data => console.log(data));
+  // axios.get('http://localhost:3001dishes')
+  // .then(response => response.data)
+  // .then(data => console.log(data))
+  // .catch(error => console.log(error.message));
+
+  // axios.post('http://localhost:3001/dishes', {name:"pizza",price:'799'})
+  // .then(response => console.log(response));
+
+  // axios.put('http://localhost:3001/dishes/1', {name:"Cheese Cake",price:'250'})
+  // .then(response => console.log(response));
+
+  axios.delete('http://localhost:3001/dishes/5')
+  .then(response => console.log(response));
   }
 
   render(){
