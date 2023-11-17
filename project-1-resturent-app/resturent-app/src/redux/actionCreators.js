dabdf93778560b138426673a55ad47205c1949f2
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2023-11-17 21:59:17 +0600

    3. Fetch Comments from Server

--- project-1-resturent-app/resturent-app/src/redux/actionCreators.js
+++ project-1-resturent-app/resturent-app/src/redux/actionCreators.js
@@ -12,6 +12,21 @@ export const addComment = (dishId, rating, author, comment) => ({
     }
 })
 
+export const commentLoading = () => ({
+    type: actionTypes.COMMENT_LOADING
+})
+
+export const loadComments = comments => ({
+    type: actionTypes.LOAD_COMMENTS,
+    payload: comments
+})
+export const fetchComments = () => dispatch => {
+    dispatch(commentLoading());
+    axios.get(baseUrl+"comments")
+    .then(response => response.data)
+    .then(comments => dispatch(loadComments(comments)))
+
+}
 export const loadDishes = dishes => ({
     type: actionTypes.LOAD_DISHES,
     payload: dishes
@@ -23,7 +38,7 @@ export const dishesLoading = () => ({
 export const fetchDishes = () => dispatch => {
     dispatch(dishesLoading());
 
-   axios.get(baseUrl+"dishes")
-   .then(response => response.data)
-   .then(dishes => dispatch(loadDishes(dishes)))
+    axios.get(baseUrl + "dishes")
+        .then(response => response.data)
+        .then(dishes => dispatch(loadDishes(dishes)))
 }
