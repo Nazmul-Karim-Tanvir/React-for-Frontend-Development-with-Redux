33e2302ba3b246558f336fc71f8e75efc1396c33
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2023-11-18 23:36:05 +0600

    5 Controls Component

--- project-2-my-burger/my-burger/src/Components/Main.js
+++ project-2-my-burger/my-burger/src/Components/Main.js
@@ -3,14 +3,14 @@ import Header from './Header/Header';
 import BurgerBuilder from './BurgerBuilder/BurgerBuilder';
 
 const Main = props => {
-  return (
-    <div>
-      <Header />
-      <div className='container'>
-        <BurgerBuilder />
-      </div>
-    </div>
-  )
+    return (
+        <div>
+            <Header />
+            <div className="container">
+                <BurgerBuilder />
+            </div>
+        </div>
+    )
 }
 
 export default Main;
