c824214b802a92074602c7f1ee6db56e85081583
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2023-11-18 22:01:25 +0600

    4 Show Ingredients according to State

--- project-2-my-burger/my-burger/src/Components/BurgerBuilder/Burger/Burger.js
+++ project-2-my-burger/my-burger/src/Components/BurgerBuilder/Burger/Burger.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import Ingredient from '../Ingredient/Ingredient'
-
+import './Burger.css'
 const Burger = props => {
+  let IngredientArr = props.ingredients.map(item => {
+    let amountArr = [...Array(item.amount).keys()]
+    return amountArr.map(_ => {
+      return <Ingredient type={item.type} key={Math.random()} />
+    })
+  })
+    .reduce((arr, element) => {
+      return arr.concat(element);
+    }, []);
+  if (IngredientArr.length === 0) {
+    IngredientArr = <p>Please add some ingredients!</p>
+  }
   return (
-    <div>
-      <Ingredient type="bread-top"/>
-      <Ingredient type="cheese"/>
-      <Ingredient type="salad"/>
-      <Ingredient type="meat"/>
-      <Ingredient type="bread-bottom"/>
+    <div className='Burger'>
+      <Ingredient type="bread-top" />
+      {IngredientArr}
+      <Ingredient type="bread-bottom" />
     </div>
   )
 }
