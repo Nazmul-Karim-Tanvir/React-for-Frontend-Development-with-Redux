d8a9f54335d7a0738f821b03d77a99b0ebc21b00
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2023-11-24 21:59:36 +0600

    1 Creating the Auth Form using Formik

--- project-2-my-burger/my-burger/src/Components/Main.js
+++ project-2-my-burger/my-burger/src/Components/Main.js
@@ -4,6 +4,7 @@ import BurgerBuilder from './BurgerBuilder/BurgerBuilder';
 import { Route, Routes } from 'react-router-dom';
 import Orders from './Orders/Orders';
 import Checkout from './Orders/Checkout/Checkout';
+import Auth from './Auth/Auth';
 
 const Main = (props) => {
 	return (
@@ -14,6 +15,7 @@ const Main = (props) => {
 					<Route path="/" element={<BurgerBuilder />} />
 					<Route path="/orders" element={<Orders />} />
 					<Route path="/checkout" element={<Checkout />} />
+					<Route path="/login" element={<Auth />} />
 				</Routes>
 			</div>
 		</div>
